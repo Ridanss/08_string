9fe83e698bb4b74089a966adb1ac2986126ecac5
Karel <pavlonk.04@spst.eu>
2022-03-24 10:46:23 +0100

    p01 hotovo

--- P01/Form1.Designer.cs
+++ P01/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonExecute = new System.Windows.Forms.Button();
+            this.labelZnaky = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(38, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonExecute
+            // 
+            this.buttonExecute.Location = new System.Drawing.Point(211, 30);
+            this.buttonExecute.Name = "buttonExecute";
+            this.buttonExecute.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute.TabIndex = 1;
+            this.buttonExecute.Text = "zadat";
+            this.buttonExecute.UseVisualStyleBackColor = true;
+            this.buttonExecute.Click += new System.EventHandler(this.buttonExecute_Click);
+            // 
+            // labelZnaky
+            // 
+            this.labelZnaky.AutoSize = true;
+            this.labelZnaky.Location = new System.Drawing.Point(35, 70);
+            this.labelZnaky.Name = "labelZnaky";
+            this.labelZnaky.Size = new System.Drawing.Size(41, 13);
+            this.labelZnaky.TabIndex = 2;
+            this.labelZnaky.Text = "Znaků:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(426, 265);
+            this.Controls.Add(this.labelZnaky);
+            this.Controls.Add(this.buttonExecute);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonExecute;
+        private System.Windows.Forms.Label labelZnaky;
     }
 }
 
--- P01/Form1.cs
+++ P01/Form1.cs
@@ -16,5 +16,22 @@ namespace P01
         {
             InitializeComponent();
         }
+
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            string value = textBox1.Text;
+            string samohlasky = "aeiouyáéěíóúůý";
+            int samohlasek = 0;
+            string souhlasky = "bcčdďfghjklmnpqrřsštťvwxzž";
+            int souhlasek = 0;
+            value = value.ToLower();
+            foreach (char znak in value)
+            {
+                if (samohlasky.Contains(znak)) samohlasek++;
+                else if (souhlasky.Contains(znak)) souhlasek++;
+            }
+            int nepismenych = value.Length - (souhlasek + samohlasek);
+            labelZnaky.Text = string.Format("Znaky:\n{0} samohlásek\n{1} souhlásek\n{2} ostatních znaků", samohlasek, souhlasek, nepismenych);
+        }
     }
 }
