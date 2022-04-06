1c6abaa631b315ab8c5b06cd5e2c1f94704d1c52
Karel <pavlonk.04@spst.eu>
2022-04-06 08:55:28 +0200

    p06

---
+++ P06/Form6.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace P06
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonReplace = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "     ano je   to    tak je   to   votom    ";
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(33, 63);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(103, 23);
+            this.buttonRemove.TabIndex = 1;
+            this.buttonRemove.Text = "Remove space";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonReplace
+            // 
+            this.buttonReplace.Location = new System.Drawing.Point(33, 92);
+            this.buttonReplace.Name = "buttonReplace";
+            this.buttonReplace.Size = new System.Drawing.Size(103, 23);
+            this.buttonReplace.TabIndex = 2;
+            this.buttonReplace.Text = "Replace space";
+            this.buttonReplace.UseVisualStyleBackColor = true;
+            this.buttonReplace.Click += new System.EventHandler(this.buttonReplace_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(33, 121);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(383, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 217);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.buttonReplace);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form6";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonReplace;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ P06/Form6.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P06
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            string chain = textBox1.Text;
+            chain = chain.Trim();
+            char prevVal = chain[1];
+            int i = 2;
+            while (i < chain.Length)
+            {
+                if (chain[i] == ' ' && prevVal == ' ')
+                {
+                    chain = chain.Remove(i, 1);
+                }
+                else
+                {
+                    prevVal = chain[i];
+                    i++;
+                }
+            }
+            textBox2.Text = chain;
+        }
+
+        private void buttonReplace_Click(object sender, EventArgs e)
+        {
+            string chain = textBox1.Text;
+            chain = chain.Trim();
+            while (chain.Contains("  "))
+            {
+                chain = chain.Replace("  ", " ");
+            }
+            textBox2.Text = chain;
+        }
+    }
+}
