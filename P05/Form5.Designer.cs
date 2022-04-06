ab7fff1b7ca599e9e94655dd29810271b80dd90a
Karel <pavlonk.04@spst.eu>
2022-04-06 08:33:23 +0200

    p05

--- P05/Form5.Designer.cs
+++ P05/Form5.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonExecute1 = new System.Windows.Forms.Button();
+            this.labelPocetSlov1 = new System.Windows.Forms.Label();
+            this.buttonExecute2 = new System.Windows.Forms.Button();
+            this.labelPocetSlov2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(52, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(339, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // buttonExecute1
+            // 
+            this.buttonExecute1.Location = new System.Drawing.Point(52, 64);
+            this.buttonExecute1.Name = "buttonExecute1";
+            this.buttonExecute1.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute1.TabIndex = 1;
+            this.buttonExecute1.Text = "zadat";
+            this.buttonExecute1.UseVisualStyleBackColor = true;
+            this.buttonExecute1.Click += new System.EventHandler(this.buttonExecute_Click);
+            // 
+            // labelPocetSlov1
+            // 
+            this.labelPocetSlov1.AutoSize = true;
+            this.labelPocetSlov1.Location = new System.Drawing.Point(133, 69);
+            this.labelPocetSlov1.Name = "labelPocetSlov1";
+            this.labelPocetSlov1.Size = new System.Drawing.Size(59, 13);
+            this.labelPocetSlov1.TabIndex = 2;
+            this.labelPocetSlov1.Text = "počet slov:";
+            // 
+            // buttonExecute2
+            // 
+            this.buttonExecute2.Location = new System.Drawing.Point(52, 93);
+            this.buttonExecute2.Name = "buttonExecute2";
+            this.buttonExecute2.Size = new System.Drawing.Size(75, 23);
+            this.buttonExecute2.TabIndex = 3;
+            this.buttonExecute2.Text = "zadat";
+            this.buttonExecute2.UseVisualStyleBackColor = true;
+            this.buttonExecute2.Click += new System.EventHandler(this.buttonExecute2_Click);
+            // 
+            // labelPocetSlov2
+            // 
+            this.labelPocetSlov2.AutoSize = true;
+            this.labelPocetSlov2.Location = new System.Drawing.Point(133, 98);
+            this.labelPocetSlov2.Name = "labelPocetSlov2";
+            this.labelPocetSlov2.Size = new System.Drawing.Size(59, 13);
+            this.labelPocetSlov2.TabIndex = 4;
+            this.labelPocetSlov2.Text = "počet slov:";
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelPocetSlov2);
+            this.Controls.Add(this.buttonExecute2);
+            this.Controls.Add(this.labelPocetSlov1);
+            this.Controls.Add(this.buttonExecute1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form5";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonExecute1;
+        private System.Windows.Forms.Label labelPocetSlov1;
+        private System.Windows.Forms.Button buttonExecute2;
+        private System.Windows.Forms.Label labelPocetSlov2;
     }
 }
 
--- P05/Form5.cs
+++ P05/Form5.cs
@@ -16,5 +16,26 @@ namespace P05
         {
             InitializeComponent();
         }
+
+        private void buttonExecute_Click(object sender, EventArgs e)
+        {
+            string chain = textBox1.Text;
+            string[] words = chain.Split(' ');
+            int count = words.Length;
+            labelPocetSlov1.Text = ("počet slov: " + count);
+        }
+
+        private void buttonExecute2_Click(object sender, EventArgs e)
+        {
+            string chain = textBox1.Text;
+            chain = chain.Trim();
+            string[] words = chain.Split(' ');
+            int count = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] != string.Empty) count++;
+            }
+            labelPocetSlov2.Text = ("počet slov: " + count);
+        }
     }
 }
