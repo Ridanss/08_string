9cb8bdaa544227cd8b17ab83e58f7c45a84aa89e
Karel <pavlonk.04@spst.eu>
2022-04-07 11:22:38 +0200

    p08

--- P08/Form8.Designer.cs
+++ P08/Form8.Designer.cs
@@ -154,7 +154,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(562, 348);
             this.Controls.Add(this.labelCheckedFinish);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.buttonExecute);
