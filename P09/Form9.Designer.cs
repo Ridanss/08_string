a9d86c47fefda2d6e18ae97396481772e4d0bcf7
Karel <pavlonk.04@spst.eu>
2022-04-07 11:31:12 +0200

    p09 beginning

---
+++ P09/Form9.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace P09
+{
+    partial class Form9
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonCorrect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 32);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(389, 198);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "no tak to ne....\r\nA\r\nje to no...\r\nale neni mu nic";
+            // 
+            // buttonCorrect
+            // 
+            this.buttonCorrect.Location = new System.Drawing.Point(427, 48);
+            this.buttonCorrect.Name = "buttonCorrect";
+            this.buttonCorrect.Size = new System.Drawing.Size(98, 30);
+            this.buttonCorrect.TabIndex = 1;
+            this.buttonCorrect.Text = "Opravit";
+            this.buttonCorrect.UseVisualStyleBackColor = true;
+            this.buttonCorrect.Click += new System.EventHandler(this.buttonCorrect_Click);
+            // 
+            // Form9
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCorrect);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form9";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button buttonCorrect;
+    }
+}
+
---
+++ P09/Form9.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P09
+{
+    public partial class Form9 : Form
+    {
+        public Form9()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCorrect_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < textBox1.Lines.Count(); i++)
+            {
+                string chain = textBox1.Lines[i];
+                
+            }
+        }
+    }
+}
